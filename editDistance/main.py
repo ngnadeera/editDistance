f1cfadd87ddeb6ecd125d9dc0beee6461f5a4384
nadeera gayantha <nadeeragyantha@gmail.com>
2024-03-26 15:44:23 +0530

    first commit

---
+++ editDistance/main.py
@@ -0,0 +1,41 @@
+
+def computeEditDistance (s,t):
+
+    '''
+
+    :param s: s word
+    :param t: t word
+    :return: minimum number of letters needed to be changed in order to make s equal to t
+    '''
+
+    def recurse(m,n):
+
+        '''
+
+        :param m: len of s
+        :param n: len of t
+        :return: minimum number of letters needed to be changed in order to make s equal to t
+        '''
+
+        if m == 0:
+            return n
+        elif n == 0:
+            return m
+        elif s[m-1] == t[n-1]:
+            return recurse(m-1,n-1)
+        else:
+            subCost = 1 + recurse(m-1,n-1)
+            insCost = 1 + recurse(m,n-1)
+            delCost = 1 + recurse(m-1,n)
+
+            result = min(subCost,insCost,delCost)
+            return result
+    return recurse(len(s),len(t))
+
+
+if __name__ == '__main__':
+    result = computeEditDistance("cat", "cats")
+    print("result: ", result)
+
+
+
